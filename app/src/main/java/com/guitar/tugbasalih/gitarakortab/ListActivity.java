42c0b563636dcb094ef25c9e7d7e15941973f5ef
salihklc <salihklc91@gmail.com>
2014-12-23 20:51:54 +0200

    Xml tasarımları hazırlandı test edildi Listview de sıkıntı var...

---
+++ app/src/main/java/com/guitar/tugbasalih/gitarakortab/ListActivity.java
@@ -0,0 +1,23 @@
+package com.guitar.tugbasalih.gitarakortab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by salih on 12/23/14.
+ */
+public class ListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_list);
+        Intent intent = getIntent();
+        ListView songList   =   (ListView)  findViewById(R.id.songList);
+        String value = intent.getStringExtra("key");
+        Toast.makeText(getApplicationContext(),value,Toast.LENGTH_LONG).show();
+    }
+}
